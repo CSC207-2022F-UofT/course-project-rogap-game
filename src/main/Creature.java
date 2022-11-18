da28f97714c694be818cf4dd838c6dc5f44a0225
khushil-sketch <khushiln6@gmail.com>
2022-11-17 22:15:39 -0500

    Created the Creature Abstract class

---
+++ src/main/Creature.java
@@ -0,0 +1,13 @@
+public  abstract class Creature {
+    public abstract void Creature();
+
+    public abstract void setMovableBehavior();
+
+    public abstract void setHealth(); // Set the Initial Creature Health
+
+    public abstract void getHealth(); // Get the creature's current Health
+
+    public abstract void reduceHealth();
+
+    public abstract void die();
+}

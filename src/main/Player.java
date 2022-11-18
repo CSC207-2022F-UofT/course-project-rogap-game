40ef52266fac3973052986c1e7deb93c08d38efa
khushil-sketch <khushiln6@gmail.com>
2022-11-17 22:36:25 -0500

    created the player class and inherited the creature abstract class methods

--- src/main/Player.java
+++ src/main/Player.java
@@ -1,2 +1,36 @@
 public class Player extends Creature{
+    public Player() {
+
+    }
+
+
+    @Override
+    public void Creature() {
+
+    }
+
+    @Override
+    public void setMovableBehavior() {
+
+    }
+
+    @Override
+    public void setHealth() {
+
+    }
+
+    @Override
+    public void getHealth() {
+
+    }
+
+    @Override
+    public void reduceHealth() {
+
+    }
+
+    @Override
+    public void die() {
+
+    }
 }

4d80d11f08dbc5e854035c8d42929e14be35d6f4
qwet1235 <gabriela.jang@mail.utoronto.ca>
2022-11-29 23:37:35 -0500

    again, GameplayScreen has some errors but these will be changed once the final Entities are finalized.

--- src/main/java/screens/GameplayScreen.java
+++ src/main/java/screens/GameplayScreen.java
@@ -4,8 +4,11 @@ package screens;
 
 import player_attack_use_case.PlayerAttackPresenter;
 import player_attack_use_case.PlayerAttackResponseModel;
+import monster_attack_use_case.MonsterAttackPresenter;
+import monster_attack_use_case.MonsterAttackResponseModel;
 
-public class GameplayScreen implements PlayerAttackPresenter {
+public class GameplayScreen implements PlayerAttackPresenter, MonsterAttackPresenter {
+    // From PlayerAttackPresenter
     @Override
     public PlayerAttackResponseModel preparePlayerAttackView(PlayerAttackResponseModel playerAttackResponseModel) {
         playerAttackResponseModel.getPlayer().update(); // update with new inputs (player.attacking(true)
@@ -24,6 +27,16 @@ public class GameplayScreen implements PlayerAttackPresenter {
         return null;
     }
 
+    // From MonsterAttackPresenter
+    @Override
+    public MonsterAttackResponseModel prepareMonsterAttackView(MonsterAttackResponseModel monsterAttackResponseModel) {
+        monsterAttackResponseModel.getMonster().update();
+        return null;
+    }
 
-
+    @Override
+    public MonsterAttackResponseModel preparePlayerHitView(MonsterAttackResponseModel monsterAttackResponseModel) {
+        monsterAttackResponseModel.getPlayer().update();
+        return null;
+    }
 }

afa6df516d5bfeab0c827db10f3f43091ef3dc5b
Raiyan <raiyankr@gmail.com>
2022-12-02 01:01:36 -0500

    - Added Monster Super class

---
+++ src/main/java/Entities/Monster.java
@@ -0,0 +1,73 @@
+package Entities;
+
+import main.GamePanel;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Monster{
+    private GamePanel gamePanel;
+    private int xEnemy;
+    private int yEnemy;
+    private double distance;
+    private int velX = 0, velY = 0;
+    private int spawnX, spawnY;
+    private Rectangle hitBox;
+
+    public Monster(GamePanel gamePanel, int x, int y, int spawnX, int spawnY) {
+        this.gamePanel = gamePanel;
+        this.spawnY = spawnY;
+        this.spawnX = spawnX;
+        this.xEnemy = x + this.spawnX;
+        this.yEnemy = y + this.spawnY;
+    }
+    public void update() {
+        if (!getHitBox().intersects(gamePanel.player.getHitBox())){
+            enemyMovement();
+        }
+    }
+    public Rectangle getHitBox() {
+        hitBox = new Rectangle(spawnX - 1280 + 4, spawnY - 720 + 4, 24, 24);
+        return hitBox;
+    }
+
+    private void enemyMovement() { //In order to update current enemy location must update absXenemy.
+        distance = Math.sqrt((Math.pow((gamePanel.player.getAbsXPlayer() - xEnemy - spawnX + 1896),2) + Math.pow((gamePanel.player.getAbsYPlayer() - yEnemy -spawnY + 1046), 2)));
+        if (distance < 600 & distance > 110) {
+            System.out.println(yEnemy);
+            velX = enemyMoveHelper(xEnemy - 616 - 1280,gamePanel.player.getAbsXPlayer() - spawnX);
+            velY = enemyMoveHelper(yEnemy - 326 - 720,gamePanel.player.getAbsYPlayer() - spawnY);
+            ArrayList wall = currMoveCollision(velX, velY);
+            if ((Boolean) wall.get(0)) {
+                xEnemy -= velX;
+                spawnX -= velX;
+                yEnemy -= velY;
+                spawnY -= velY;
+            } else {
+                //TODO: make enemies move randomly while it is touching the border.
+            }
+        } else if (distance < 110) {
+            //TODO: add a function that calls the attack for enemy.
+        } else {
+            //TODO: make enemies move randomly while player is not close
+        }
+    }
+
+    private ArrayList currMoveCollision(int x, int y) {
+        return gamePanel.player.getWallCollision().moveAbleWall(xEnemy + 4, yEnemy + 4,
+                x, y, 24, 24);
+    }
+    private int enemyMoveHelper(int c, int targetC) {
+        if (c < targetC) {
+            return -1;
+        } else if (c == targetC) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    public int getXEnemy() {return this.xEnemy;}
+    public int getYEnemy() {return this.yEnemy;}
+    public void changeXEnemy(int x) {this.xEnemy += x;}
+    public void changeYEnemy(int y) {this.yEnemy += y;}
+}
